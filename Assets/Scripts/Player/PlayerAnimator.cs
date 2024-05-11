31f480f14b95a407101a1f879b0825fef7d79131
Maybeonce-Mini <a-u-rom@yandex.ru>
2024-05-12 00:08:05 +0400

    +

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _animator.SetTrigger("Idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _animator.SetTrigger("Run");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _animator.SetTrigger("Jump");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            _animator.SetTrigger("Attack");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            _animator.SetTrigger("Dead");
+
+        }
+    }
+}

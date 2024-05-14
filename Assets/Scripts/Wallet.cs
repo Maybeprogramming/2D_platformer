7db496c81ccfd2fc0fc7f5201844eb357bad5238
Maybeonce <a-u-rom@yandex.ru>
2024-05-14 23:52:21 +0400

    +

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -19,15 +19,13 @@ public class Wallet : MonoBehaviour
         _textCoinAmount.text = _textCoinAmount.text + " " + _amount.ToString();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null && collision.transform.TryGetComponent<Coin>(out Coin coin) == true)
         {
             _amount++;
             CoinAmountChanged?.Invoke(_amount);
-
             _textCoinAmount.text = _text + " " + _amount.ToString();
-
             coin.gameObject.SetActive(false);
         }
     }

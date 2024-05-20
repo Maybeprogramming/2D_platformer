4bba4b9fa0d638e9222fed1d6221553a4dd122e4
Maybeonce <a-u-rom@yandex.ru>
2024-05-20 23:13:01 +0400

    +

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,8 +6,10 @@ public class Attacker : MonoBehaviour
     [SerializeField] private Enemy _target;
     [SerializeField] private float _damage;
     [SerializeField] private Transform _attackPoint;
+    [SerializeField] private Transform _defaultAttackPoint;
     [SerializeField] private float _attackRadius;
     [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private float _offset;
 
     private void Awake()
     {
@@ -17,27 +19,32 @@ public class Attacker : MonoBehaviour
     private void OnEnable()
     {
         _playerInput.AttackButtonDowned += OnAttacked;
+        _playerInput.DirectionMoving += OnSetPointToAttack;
     }
 
     private void OnDisable()
     {
         _playerInput.AttackButtonDowned -= OnAttacked;
+        _playerInput.DirectionMoving -= OnSetPointToAttack;
     }
 
     private void OnAttacked()
     {
         Collider2D enemy = Physics2D.OverlapCircle(new Vector2(_attackPoint.position.x, _attackPoint.position.y), _attackRadius, _layerMask);
 
-
         if (enemy != null && enemy.TryGetComponent<Enemy>(out _target) == true)
         {
-            Debug.Log(enemy.name);
-
             if (_target != null && _target.TryGetComponent<Health>(out Health enemyHealth))
             {
                 enemyHealth.Remove(_damage);
             }
         }
+    }
+
+    private void OnSetPointToAttack(Vector2 direction)
+    {
+        float horizontalPosition = direction.x * _offset;
+        _attackPoint.transform.position = new Vector3(_defaultAttackPoint.position.x + horizontalPosition, _attackPoint.position.y, 0f);
     }
 
     private void OnDrawGizmos()

c9ca64f17202ab0c07290fb809b4652e85046905
Maybeonce <a-u-rom@yandex.ru>
2024-05-31 15:38:32 +0300

    +

---
+++ Assets/Task_MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    [SerializeField] private string _animationTrigger;
+
+    private void Update()
+    {
+    }
+
+    private void OnAnimationEnter(PointerEventData data)
+    {
+
+    }
+}

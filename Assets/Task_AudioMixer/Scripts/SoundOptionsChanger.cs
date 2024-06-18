33e429aef35442c9528cca76f9b13148e14dd48f
Maybeonce <a-u-rom@yandex.ru>
2024-06-18 11:35:31 +0300

    +

--- Assets/Task_AudioMixer/Scripts/SoundOptionsChanger.cs
+++ Assets/Task_AudioMixer/Scripts/SoundOptionsChanger.cs
@@ -1,49 +1,86 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SoundOptionsChanger : MonoBehaviour
 {
+    private const string MasterVolumeTag = "MasterVolume";
+    private const string ButtonsVolumeTag = "ButtonsVolume";
+    private const string MusicVolumeTag = "MusicVolume";
+
     [SerializeField] private AudioMixer _audioMixer;
-    [SerializeField] private AudioSource _AudioSource;
+    [SerializeField] private AudioSource _audioSourceMusic;
+    [SerializeField] private AudioSource _audioSourceButton1;
+    [SerializeField] private AudioSource _audioSourceButton2;
+    [SerializeField] private AudioSource _audioSourceButton3;
     [SerializeField] private AudioClip _musicAudioClip;
     [SerializeField] private AudioClip _buttonAudioClip1;
     [SerializeField] private AudioClip _buttonAudioClip2;
     [SerializeField] private AudioClip _buttonAudioClip3;
-
-    [SerializeField] private Button _SoundButton1;
-    [SerializeField] private Button _SoundButton2;
-    [SerializeField] private Button _SoundButton3;
-    [SerializeField] private Button _MasterVolumeButton;
-
-    [SerializeField] private Slider _masterVolume;
-    [SerializeField] private Slider _musicVolume;
-    [SerializeField] private Slider _buttonsVolume;
+    [SerializeField] private Button _soundButton1;
+    [SerializeField] private Button _soundButton2;
+    [SerializeField] private Button _soundButton3;
+    [SerializeField] private Button _masterVolumeButton;
+    [SerializeField] private Slider _masterVolumeSlider;
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Slider _buttonsVolumeSlider;
 
     private float masterVolumeLevel;
     private float musicVolumeLevel;
     private float buttonsVolumeLevel;
 
-    public float GetNormalizedSoundValue(float value)
+    private void Start()
     {
-        return Mathf.Log10(value) * 20;
+        //_audioSourceMusic.clip = _musicAudioClip;
+        //_audioSourceButton1.clip = _buttonAudioClip1;
+        //_audioSourceButton2.clip = _buttonAudioClip2;
+        //_audioSourceButton3.clip = _buttonAudioClip3;
+
+        _soundButton1.onClick.AddListener(OnPlayOneShootSound);
+        _soundButton2.onClick.AddListener(OnPlayOneShootSound);
+        _soundButton3.onClick.AddListener(OnPlayOneShootSound);
+        _masterVolumeButton.onClick.AddListener(OnSetMasterSound);
     }
 
     public void SetMasterVolume()
     {
-        masterVolumeLevel = GetNormalizedSoundValue(_masterVolume.value);
-        _audioMixer.SetFloat("MasterVolume", masterVolumeLevel);
+        masterVolumeLevel = GetNormalizedSoundValue(_masterVolumeSlider.value);
+        _audioMixer.SetFloat(MasterVolumeTag, masterVolumeLevel);
     }
+
 
     public void SetMusicVolume()
     {
-        musicVolumeLevel = GetNormalizedSoundValue(_musicVolume.value);
-        _audioMixer.SetFloat("MusicVolume", musicVolumeLevel);
+        musicVolumeLevel = GetNormalizedSoundValue(_musicVolumeSlider.value);
+        _audioMixer.SetFloat(MusicVolumeTag, musicVolumeLevel);
     }
 
-    public void SetSfxVolume()
+    public void SetButtonsSoundVolume()
     {
-        buttonsVolumeLevel = GetNormalizedSoundValue(_buttonsVolume.value);
-        _audioMixer.SetFloat("ButtonsVolume", buttonsVolumeLevel);
+        buttonsVolumeLevel = GetNormalizedSoundValue(_buttonsVolumeSlider.value);
+        _audioMixer.SetFloat(ButtonsVolumeTag, buttonsVolumeLevel);
+    }
+
+    private float GetNormalizedSoundValue(float value)
+    {
+        return Mathf.Log10(value) * 20;
+    }
+
+    private void OnPlayOneShootSound()
+    {
+        Debug.Log("Play One Shoot");
+    }
+
+    private void OnSetMasterSound()
+    {
+        _audioSourceButton1.mute = !_audioSourceButton1.mute;
+        _audioSourceButton2.mute = !_audioSourceButton2.mute;
+        _audioSourceButton3.mute = !_audioSourceButton3.mute;
+        _audioSourceMusic.mute = !_audioSourceMusic.mute;
+
+        TMP_Text text = _masterVolumeButton.GetComponent<TMP_Text>();
+        text.text = _audioSourceButton1.mute == false ? "Turn sound Off" : "Turn sound On";
     }
 }

35042bf6841ca6a988611f75fec93b7cb43e00cf
Maybeonce <a-u-rom@yandex.ru>
2024-07-07 13:22:43 +0400

    +

---
+++ Assets/Task_HealthBar/Scripts/HealthPoint.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class HealthPoint : MonoBehaviour
+{
+    [SerializeField] private float _currentValue;
+    [SerializeField] private float _minValue = 0;
+    [SerializeField] private float _maxValue;
+
+    private float _newValue;
+
+    public event Action<float> ValueChanged;
+
+    public float MaxValue => _maxValue;
+    public float MinValue => _minValue;
+    public float CurrentValue => _currentValue;
+
+    private void Awake()
+    {
+        _currentValue = _maxValue;
+    }
+
+    public bool Add(float healthValue)
+    {
+        _newValue = Mathf.Clamp(_currentValue + healthValue, _minValue, _maxValue);
+
+        if (_currentValue != _newValue)
+        {
+            _currentValue = _newValue;
+            ValueChanged?.Invoke(_currentValue);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool Remove(float healthValue)
+    {
+        _newValue = Mathf.Clamp(_currentValue - healthValue, _minValue, _maxValue);
+
+        if (_currentValue != _newValue)
+        {
+            _currentValue = _newValue;
+            ValueChanged?.Invoke(_currentValue);
+
+            return true;
+        }
+
+        return false;
+    }
+}
